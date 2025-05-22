747fa9d2d598190382b3d78fcbd66f9677106c75
xamenyap <jpham2811@gmail.com>
2025-05-22 22:12:49 +0800

    refactor into functions

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,58 @@ impl PartialOrd for TodoItem {
 use std::io;
 use std::io::Write;
 
+fn display_todo_items() {
+    let todo_file_path = "todo.txt";
+    let contents = fs::read_to_string(todo_file_path).expect("Unable to read file");
+
+    let mut todo_items: Vec<TodoItem> = contents
+        .lines()
+        .map(|line| {
+            let parts: Vec<&str> = line.splitn(2, "  ").collect();
+            let date = NaiveDate::parse_from_str(parts[0], "%Y-%m-%d").expect("Invalid date format");
+            let task = parts[1].trim().to_string();
+            TodoItem { date, task }
+        })
+        .collect();
+
+    todo_items.sort();
+
+    if todo_items.is_empty() {
+        println!("There is no items in your todo list");
+    } else {
+        println!("----------------------------------");
+        println!("| Date       | Task                |");
+        println!("----------------------------------");
+        for item in todo_items {
+            println!("| {} | {} |", item.date.format("%Y-%m-%d"), item.task);
+        }
+        println!("----------------------------------");
+    }
+}
+
+fn add_todo_item() {
+    print!("Enter new todo item: ");
+    io::stdout().flush().unwrap();
+
+    let mut new_todo = String::new();
+    io::stdin().read_line(&mut new_todo).expect("Failed to read line");
+    let new_todo = new_todo.trim();
+
+    let current_date = chrono::Local::now().naive_local().date();
+    let todo_string = format!("{}  {}\n", current_date.format("%Y-%m-%d"), new_todo);
+
+    let todo_file_path = "todo.txt";
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(todo_file_path)
+        .expect("Failed to open file");
+
+    file.write_all(todo_string.as_bytes()).expect("Failed to write to file");
+
+    println!("Todo item added.");
+}
+
 fn main() {
     loop {
         println!("----------------------------------");
@@ -38,56 +90,8 @@ fn main() {
         io::stdin().read_line(&mut choice).expect("Failed to read line");
 
         match choice.trim() {
-            "1" => {
-                let todo_file_path = "todo.txt";
-                let contents = fs::read_to_string(todo_file_path).expect("Unable to read file");
-
-                let mut todo_items: Vec<TodoItem> = contents
-                    .lines()
-                    .map(|line| {
-                        let parts: Vec<&str> = line.splitn(2, "  ").collect();
-                        let date = NaiveDate::parse_from_str(parts[0], "%Y-%m-%d").expect("Invalid date format");
-                        let task = parts[1].trim().to_string();
-                        TodoItem { date, task }
-                    })
-                    .collect();
-
-                todo_items.sort();
-
-                if todo_items.is_empty() {
-                    println!("There is no items in your todo list");
-                } else {
-                    println!("----------------------------------");
-                    println!("| Date       | Task                |");
-                    println!("----------------------------------");
-                    for item in todo_items {
-                        println!("| {} | {} |", item.date.format("%Y-%m-%d"), item.task);
-                    }
-                    println!("----------------------------------");
-                }
-            }
-            "2" => {
-                print!("Enter new todo item: ");
-                io::stdout().flush().unwrap();
-
-                let mut new_todo = String::new();
-                io::stdin().read_line(&mut new_todo).expect("Failed to read line");
-                let new_todo = new_todo.trim();
-
-                let current_date = chrono::Local::now().naive_local().date();
-                let todo_string = format!("{}  {}\n", current_date.format("%Y-%m-%d"), new_todo);
-
-                let todo_file_path = "todo.txt";
-                let mut file = fs::OpenOptions::new()
-                    .write(true)
-                    .append(true)
-                    .open(todo_file_path)
-                    .expect("Failed to open file");
-
-                file.write_all(todo_string.as_bytes()).expect("Failed to write to file");
-
-                println!("Todo item added.");
-            }
+            "1" => display_todo_items(),
+            "2" => add_todo_item(),
             "3" => {
                 println!("Exiting program.");
                 break;
