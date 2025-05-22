2c5c8057af276eddbb354ad62c815279922822b6
xamenyap <jpham2811@gmail.com>
2025-05-22 21:55:26 +0800

    add ability to add new todo item

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,8 @@ fn main() {
     loop {
         println!("----------------------------------");
         println!("1: Display Todo Items");
-        println!("2: Exit");
+        println!("2: Add Todo Item");
+        println!("3: Exit");
         println!("----------------------------------");
 
         print!("Enter your choice: ");
@@ -62,11 +63,33 @@ fn main() {
                 println!("----------------------------------");
             }
             "2" => {
+                print!("Enter new todo item: ");
+                io::stdout().flush().unwrap();
+
+                let mut new_todo = String::new();
+                io::stdin().read_line(&mut new_todo).expect("Failed to read line");
+                let new_todo = new_todo.trim();
+
+                let current_date = chrono::Local::now().naive_local().date();
+                let todo_string = format!("{}  {}\n", current_date.format("%Y-%m-%d"), new_todo);
+
+                let todo_file_path = "todo.txt";
+                let mut file = fs::OpenOptions::new()
+                    .write(true)
+                    .append(true)
+                    .open(todo_file_path)
+                    .expect("Failed to open file");
+
+                file.write_all(todo_string.as_bytes()).expect("Failed to write to file");
+
+                println!("Todo item added.");
+            }
+            "3" => {
                 println!("Exiting program.");
                 break;
             }
             _ => {
-                println!("Invalid input. Please enter 1 or 2.");
+                println!("Invalid input. Please enter 1, 2, or 3.");
             }
         }
     }
